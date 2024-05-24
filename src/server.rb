542acbd4bc9ec10db7d80c0c72e2cc5871cb92be
mat5b <briccom21@gmail.com>
2024-05-24 12:00:57 -0300

    Implementacion de /learn

--- src/server.rb
+++ src/server.rb
@@ -5,7 +5,6 @@ require 'sinatra/activerecord'
 
 require 'sinatra/reloader' if Sinatra::Base.environment == :development
 
-
 require_relative 'models/account'
 
 class App < Sinatra::Application
@@ -21,6 +20,9 @@ class App < Sinatra::Application
     set :logger, logger
   end
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   set :views, File.join(File.dirname(__FILE__), 'views')
   set :public_folder, File.join(File.dirname(__FILE__), 'styles')
 
@@ -31,11 +33,14 @@ class App < Sinatra::Application
     end
   end
 
+  # app.rb
+
+  AVAILABLE_GAMES = ['Counter Strike', 'Game 2', 'Game 3'].freeze
+
   get '/login' do
     error_message = params[:error]
     erb :login, locals: { error_message: error_message }
   end
-
 
   get '/' do
     redirect '/login'
@@ -46,7 +51,7 @@ class App < Sinatra::Application
   end
 
   get '/home' do
-    'Home'
+    erb :home
   end
 
   post '/login' do
@@ -56,21 +61,18 @@ class App < Sinatra::Application
     account = Account.find_by(nickname: nickname, password: password)
 
     if account
-
+      session[:email] = account.email
       redirect '/home'
     else
       redirect '/login?error=Invalid-username-or-password'
     end
   end
 
-
-
   post '/signup' do
     email = params[:email]
     password = params[:password]
     name = params[:name]
     nickname = params[:nickname]
-
 
     if Account.exists?(email: email)
       redirect '/signup?error=Email-already-exists'
@@ -83,15 +85,35 @@ class App < Sinatra::Application
     account = Account.new(email: email, password: password, name: name, nickname: nickname, progress: 0)
 
     if account.save
+      session[:logged_in] = true
+      session[:account_id] = account.id
       redirect '/login'
     else
-      redirect '/signup?error=Invalid-input-format'
+      erb :signup, locals: { error_message: "Error al crear cuenta" }
     end
   end
 
+  post '/home' do
+    selected_game = params[:game]
+    session[:selected_game] = selected_game
+    redirect '/learn'
+  end
 
+  get '/learn' do
+    selected_game = session[:selected_game]
+    # Fetch questions related to the selected game from the database
+    # questions = Question.where(game: selected_game) # Assuming you have a Question model
+    erb :learn, locals: { selected_game: selected_game }
+  end
 
+  post '/learn' do
+    selected_game = session[:selected_game]
+    difficulty = params[:difficulty]
+    "Learning #{selected_game} at difficulty #{difficulty}!"
+  end
 
-
-
+  post '/logout' do
+    session.clear
+    redirect '/'
+  end
 end
